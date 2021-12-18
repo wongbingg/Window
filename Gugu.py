113ce5f6239e27c6ba27ebe2e4969854b75eb2e1
wongbing <wb277@naver.com>
2021-12-18 14:53:28 +0900

    after mcbook

--- Gugu.py
+++ Gugu.py
@@ -1,8 +1,17 @@
-def Gugu():
-    for i in range(2,10):
-        print("----%d단----"%i)
-        for j in range(1,10):
-            print("{} x {} = {}".format(i,j,i*j))
-        
-Gugu()
-    
+import sys
+n = int(sys.stdin.readline())
+rn=0
+for i in range(1,10000):
+    rn += i
+    if n<=rn:
+        apart=rn-n
+        if i ==1 or i%2==0:
+            print(i-apart,'/',apart+1,sep='')
+            
+            
+            break
+        else:
+            print(apart+1,'/',i-apart,sep='')
+            break
+
+
--- practice280.py
+++ practice280.py
@@ -1,11 +1,14 @@
-per = ["10.31", "", "8.00"]
+# per = ["10.31", "", "8.00"]
 
-for i in per:
-    try:
-        print(float(i))
-    except:
-        print("0은 입력 불가능 합니다")
-    else:
-        print("성공")
-    finally:
-        print("NEXT LEVEL")
+# for i in per:
+#     try:
+#         print(float(i))
+#     except:
+#         print("0은 입력 불가능 합니다")
+#     else:
+#         print("성공")
+#     finally:
+#         print("NEXT LEVEL")
+N = ['1','8']
+N[0].replace('1','2')
+print(N)
