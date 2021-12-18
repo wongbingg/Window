1d5c05baccfb1949c351dc4dd6a9eef2e7c8dda4
wongbingg <wb277@naver.com>
2021-12-18 20:07:16 +0900

    commit in vscode

--- Algorithm prac/baek2775.py
+++ Algorithm prac/baek2775.py
@@ -1,16 +1,17 @@
 import sys
 q = int(sys.stdin.readline())
-lst=[[1,2,3,4,5,6,7,8,9,10,11,12,13,14]]
-
+prt=[]
 for _ in range(q):
+    lst=[[1,2,3,4,5,6,7,8,9,10,11,12,13,14]]
     k = int(sys.stdin.readline())
     n = int(sys.stdin.readline())
     for j in range(k): #k층
         lst2=[]
         for i in range(n): #n호 일 때
             lst2.append(sum(lst[j][:(i+1)]))
-    lst.append(lst2)
-print(lst[k-1][n-1])
-print(lst[k][n-1])
+        lst.append(lst2)
+    prt.append(lst[k][n-1])
+for k in prt:
+    print(k)
 
 
